b31e6cdf8c88a96933d2ed0e244ee5c68207579a
Jackson Barr <jbarr39@gatech.edu>
2023-07-06 22:41:50 -0400

    Add queries for specific metadata

--- src/main/java/com/example/gametrackerapi/service/IGDBRequestHelper.java
+++ src/main/java/com/example/gametrackerapi/service/IGDBRequestHelper.java
@@ -1,4 +1,4 @@
-package com.example.gametrackerapi.util;
+package com.example.gametrackerapi.service;
 
 import com.example.gametrackerapi.core.exception.InvalidAccessKeyException;
 import org.springframework.http.*;
@@ -21,14 +21,27 @@ public class IGDBRequestHelper {
         headers.set("Authorization", "Bearer " + accessToken);
     }
 
-    public ResponseEntity<String> post(String uri, String body) {
+    public ResponseEntity<String> post(String destination, String body) {
         HttpEntity<String> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(uri, entity, String.class);
+        String baseUrl = "https://api.igdb.com/v4";
+        ResponseEntity<String> response = restTemplate.postForEntity(baseUrl + destination, entity, String.class);
 
         if (response.getStatusCode() == HttpStatus.UNAUTHORIZED || response.getStatusCode() == HttpStatus.FORBIDDEN) {
             throw new InvalidAccessKeyException();
         }
 
         return response;
+    }
+
+    public String getArtworkURL(int artworkID) {
+        String body = "fields url; where id=" + artworkID + ";";
+        ResponseEntity<String> response = post("/artworks", body);
+        return response.getBody();
+    }
+
+    public String getGenreName(int genreID) {
+        String body = "fields name; where id=" + genreID + ";";
+        ResponseEntity<String> response = post("/genres", body);
+        return response.getBody();
     }
 }
