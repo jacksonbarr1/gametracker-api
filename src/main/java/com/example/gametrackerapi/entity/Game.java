c002fbd2def73fbf39be14c84c8286275f0d5749
Jackson Barr <jbarr39@gatech.edu>
2023-07-06 22:39:53 -0400

    Populate Game fields

--- src/main/java/com/example/gametrackerapi/entity/Game.java
+++ src/main/java/com/example/gametrackerapi/entity/Game.java
@@ -1,4 +1,22 @@
 package com.example.gametrackerapi.entity;
 
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "games")
+@Data
 public class Game {
+    @Id
+    private int id;
+
+    private String name;
+
+    private String summary;
+
+    private List<String> artworkURLs;
+
+    private List<String> genres;
 }
